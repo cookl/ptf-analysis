b56bcc66018c54e85cac7a525002b294071ccaee
batman1205 <51218269+batman1205@users.noreply.github.com>
2020-06-19 14:53:09 -0400

    correction_reading the Temp file

--- temperature_reading.cpp
+++ temperature_reading.cpp
@@ -1,19 +1,55 @@
 #include "wrapper.hpp"
-
+#include "MeanRMSCalc.hpp"
+#include "ErrorBarAnalysis.hpp"
+#include "WaveformFitResult.hpp"
+#include "ScanPoint.hpp"
+#include "PTFAnalysis.hpp"
+#include "PTFQEAnalysis.hpp"
+#include "Utilities.hpp"
 #include <string>
 #include <iostream>
 #include <ostream>
 #include <fstream>
+#include <sstream>
+#include <exception>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 #include <unordered_set>
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TFile.h"
+#include "TF1.h"
+#include "TVirtualFitter.h"
+#include "TFitter.h"
+#include "TMath.h"
+#include "TStyle.h"
 
 
 using namespace std;
 
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    cerr << "Enter the run number.";
-  }
+	
+	
+  if (argc != 3) {
+      cerr << "give path to file to read" << endl;
+      cerr << "usage: temperature_reading filename.root run_number" << endl;
+      return 0;
+    }
+
+    // Get utilities
+  Utilities utils;
+
+    // Set style
+  utils.set_style();
+   
+  TFile * fin = new TFile( argv[1], "read" );
+  //string outname = string("output_0000") + argv[2] + ".root";
+  //TFile * outFile = new TFile(outname.c_str(), "NEW");
 
   vector<int> phidgets = {0, 1, 3, 4};
   vector<PTF::PMTChannel> activeChannels = {};
@@ -27,9 +63,6 @@ int main(int argc, char** argv) {
   cerr << "Num entries: " << wrapper.getNumEntries() << endl << endl;
   
   for (unsigned int i = 0; i < wrapper.getNumEntries(); i++) {
-
-    auto reading_T = wrapper.getDataForCurrentEntry(PTF::Gantry0);
-
 
       auto reading  = wrapper.getReadingTemperature(PTF::T);
 
