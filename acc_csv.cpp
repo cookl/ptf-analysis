27d5608a050c87a32e2f66a8116268c025a4c32b
batman1205 <51218269+batman1205@users.noreply.github.com>
2020-09-01 16:02:19 -0700

    Addind a file to transfert the root file into csv

---
+++ acc_csv.cpp
@@ -0,0 +1,88 @@
+#include "wrapper.hpp"
+
+#include <string>
+#include <iostream>
+#include <ostream>
+#include <fstream>
+#include <unordered_set>
+
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    cerr << "Enter the run number.";
+  }
+
+  const string run_no = argv[1];
+  string root_f = "/neut/data19/vincent/ptf-analysis-2/out_run0" + run_no + ".root";
+  string csv_f  = "/neut/data19/vincent/ptf-analysis-2/acc" + run_no + ".csv";
+
+  vector<int> phidgets = {0, 1, 100};
+  vector<PTF::PMTChannel> activeChannels = {};
+  //   {0, 3},                                                                                                                                                                                                                                
+  //   {1, 4},                                                                                                                                                                                                                                
+  //   {2, 5},                                                                                                                                                                                                                                
+  //   {3, 6},                                                                                                                                                                                                                                
+  //   {4, 7},                                                                                                                                                                                                                                
+  //   {5, 8},                                                                                                                                                                                                                                
+  //   {6, 9},                                                                                                                                                                                                                                
+  //   {7, 10}                                                                                                                                                                                                                                
+  // ;                                                                                                                                                                                                                                       
+  PTF::Wrapper wrapper = PTF::Wrapper(16384, 34, activeChannels, phidgets);
+
+  unordered_set<int> skipLines = {};// {962,1923,2884,5240,6201,9611,10572,11533,12494,13455,15811,16771};                                                                                                                                    
+
+  wrapper.openFile(root_f, "scan_tree");
+  ofstream csv(csv_f);
+
+  cerr << "Num entries: " << wrapper.getNumEntries() << endl << endl;
+
+  csv << "phid_0_accx,phid_0_accy,phid_0_accz,phid_1_accx,phid_1_accy,phid_1_accz,accx,accy,accz, " << endl;
+
+  uint32_t lines = 0;
+  const uint32_t freq = 100;
+
+  for (int i = 0; i < wrapper.getNumEntries(); i++) {
+    // cerr << "Entry " << i;                                                                                                                                                                                                                 
+    if (i % freq == 0 || i == wrapper.getNumEntries() - 1) {
+      cerr << "Entry " << i << "/" << wrapper.getNumEntries() << "\u001b[34;1m (" << (((double)i)/wrapper.getNumEntries()*100) << "%)\u001b[0m\033[K";
+      if (skipLines.find(i) != skipLines.end()) {
+        cerr << "\u001b[31;1m Skipping...\u001b[0m\r";
+        continue;
+      } else {
+        cerr << "\r";
+      }
+    }
+
+    if (skipLines.find(i) != skipLines.end()) continue;
+
+    lines++;
+    wrapper.setCurrentEntry(i);
+
+    //auto location = wrapper.getDataForCurrentEntry(PTF::Gantry1);                                                                                                                                                                           
+
+    //csv << location.x << "," << location.y << "," << location.z << ",";                                                                                                                                                                     
+
+    for (int phidget : phidgets) {
+
+	       auto reading  = wrapper.getReadingForPhidget(phidget);
+
+	       csv << reading.Ax[0] << "," << reading.Ay[0] << "," << reading.Az[0];
+
+	       if (phidget != 100) {
+	 	csv << ",";
+	       }
+	     }
+
+	     csv << endl;
+	   }
+
+	   cerr << endl << "Done. WroteZEGOAL" << lines << " lines.";
+	   csv.close();
+	 }
+
+
+
+		
